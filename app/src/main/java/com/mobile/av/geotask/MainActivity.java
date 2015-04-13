89ac872278c6823402c0bb5375a0aedae40fe53c
Anand A Chokshi <a_chokshi@hotmail.com>
2015-04-12 20:09:07 -0700

    Custom Action Bar

--- app/src/main/java/com/mobile/av/geotask/MainActivity.java
+++ app/src/main/java/com/mobile/av/geotask/MainActivity.java
@@ -1,8 +1,12 @@
 package com.mobile.av.geotask;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,7 +17,9 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setTitle("Tasks");
+        setTitle(Html.fromHtml("<font color='#12cdc2'> Tasks </font>"));
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
     }
 
 
